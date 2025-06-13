22be2d7c167d3bbbd7714842a6b9460e4cca44cc
rhythmcreative <rhythmcreativedeck@gmail.com>
2025-06-13 19:10:28 +0200

    Update install.sh

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -320,22 +320,6 @@ if [ ${flg_Service} -eq 1 ]; then
 |_ -| -_|  _| | | |  _| -_|_ -|
 |___|___|_|  \_/|_|___|___|___|
 
-EOF
-
-    while read servChk; do
-
-        if [[ $(systemctl list-units --all -t service --full --no-legend "${servChk}.service" | sed 's/^\s*//g' | cut -f1 -d' ') == "${servChk}.service" ]]; then
-            echo -e "\033[0;33m[SKIP]\033[0m ${servChk} service is active..."
-        else
-            echo -e "\033[0;32m[systemctl]\033[0m starting ${servChk} system service..."
-            sudo systemctl enable "${servChk}.service"
-            sudo systemctl start "${servChk}.service"
-        fi
-
-    done < "${scrDir}/system_ctl.lst"
-fi
-
-
 
 #-------------------------#
 # install GRUB theme      #
